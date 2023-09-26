84fffe21ebb96247f471dc0c4fd9bded353c3c96
QuenKar <47681251+QuenKar@users.noreply.github.com>
2023-09-26 17:33:13 +0800

    chore: modify datetime.sql

--- tests/cases/standalone/common/types/datetime/datetime.sql
+++ tests/cases/standalone/common/types/datetime/datetime.sql
@@ -6,8 +6,12 @@ CREATE TABLE datetimes(
 
 INSERT INTO datetimes VALUES
 ('host1', '2019-01-01 00:00:00', 1000),
-('host2', '2023-01-01 00:00:00', 2000);
+('host2', '2023-01-01 00:00:00', 2000),
+('host3', '2023-01-02 00:00:00', 3000),
+('host4', '2023-01-03 00:00:00', 4000);
 
 SELECT * FROM datetimes;
+
+SELECT * FROM datetimes WHERE event_date > '2020-01-01 00:00:00'::DateTime;
 
 DROP TABLE datetimes;
